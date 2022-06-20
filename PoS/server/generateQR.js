9e06dc4bd7edca34c843b78a326710cfd608649b
PhilMeyer <philippe@famille-meyer.net>
2022-06-20 07:25:28 +0200

    Changed destination address

--- PoS/server/generateQR.js
+++ PoS/server/generateQR.js
@@ -6,4 +6,4 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-let res = await QRCode.toFile(path.join(__dirname, 'etherAddr.png'), '0x2f1644FE81AEA69d4849D5bc8F64e8196C71B752');
+let res = await QRCode.toFile(path.join(__dirname, 'etherAddr.png'), '0x420D91590Ac92492a0f042E60BEac3b6e2FF0e9B');
