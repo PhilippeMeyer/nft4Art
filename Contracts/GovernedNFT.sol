804d4f468a7450c85f1d5fae8ef21f4b4ceade7b
PhilMeyer <philippe@famille-meyer.net>
2022-06-24 14:06:23 +0200

    Added the sales record feature

--- Contracts/GovernedNFT.sol
+++ Contracts/GovernedNFT.sol
@@ -31,11 +31,27 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 //
 // Inspired from: https://betterprogramming.pub/ethereum-erc-20-meta-transactions-4cacbb3630ee
 //
-
+// In addition to the governance part, this contract also manages tokens sales. For this purpose it implements:
+// - a receive function to notify any ether transfer
+// - a recordSale function which stores the last sale and potentially when the sale is taking place on the same network performs the transfer
+// - a saleUpdate function which updates the sale's status
+// - a getSale function which returns the status of a sale
+//
 contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
     using ECDSA for bytes32;
 
-    event HasVoted(address indexed who, uint256 indexed id);
+    event HasVoted(address indexed who, uint256 indexed tokenId);           // Emmitted when a vote has been performed on a specific token
+    event ReceivedEth(address, uint);                                       // Emmitted when some ether has been recevived
+    event Sale( uint256 indexed tokenId,                                    // Emmitted when a sale on tokenId is performed
+                bytes32 indexed buyer,                                      // Buyer's address (can also be a bitcoin address)
+                uint128 price,                                              // Price as an integer
+                uint8   decimals,                                           // Decimals applied to the price
+                bytes3  currency,                                           // Currency 3 letters Iso country code + ETH and BTC 
+                bytes1  network,                                            // Network on which the payment is performed
+                bytes1  status);                                            // Sale's status: initiated, payed, completed, ....
+    event SaleUpdate( uint256 indexed tokenId,                              // Emmitted when a sale on tokenId is performed
+                      bytes32 indexed buyer,                                // Buyer's address (can also be a bitcoin address)
+                      bytes1  status);                                      // Sale's status: initiated, payed, completed, ....
 
     struct BallotMessage {
         address     from;               // Externally-owned account (EOA) of the voter.
@@ -56,6 +72,15 @@ contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
         uint128 lastVote;               // Which the last vote this person has been participating into?
     }
 
+    struct SaleRecord {
+        bytes32 buyer;                  // Buyer's address (can also be a bitcoin address)
+        uint128 price;                  // Price as an integer
+        uint8   decimals;               // Decimals applied to the price
+        bytes3  currency;               // Currency 3 letters Iso country code + ETH and BTC 
+        bytes1  network;                // Network on which the payment is performed
+        bytes1  status;                 // Sale's status: initiated, payed, completed, ....
+    }
+
     bytes32 private constant _TYPEHASH = keccak256("BallotMessage(address from,uint128 voteId,bytes data)");
 
     mapping(address => Person) private _persons;        // persons allowed to cast a vote 
@@ -63,23 +88,62 @@ contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
     uint256 private _startTimestamp;                    // start of the current vote
     uint256 private _endTimestamp;                      // end of the current vote
     mapping(uint128 => Ballot[]) private _ballots;      // ballots organised by votes
+    mapping(uint256 => SaleRecord) private _sales;      // sale status per tokenId
 
     constructor(string memory uri_) Ownable() ERC1155(uri_) EIP712("GovernedNFT", "1.0.0") {
         _currentVoteId = 0;
     }
 
+    //
+    // mint
+    // Mints a new token
+    //
+    // Parameters: 
+    //  - tokenId: the id of token
+    //  - amount: number of tokens minted
+    //  - data: opaque data sent to the transfer function 
+    //
     function mint(uint256 id, uint256 amount, bytes memory data) public onlyOwner {
         _mint(owner(), id, amount, data);
     }
 
-    function _mintBatch(uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
+    //
+    // mintBatch
+    // Mints new tokens
+    //
+    // Parameters: 
+    //  - ids: array of the id of tokens
+    //  - amounts: array of the number of tokens minted
+    //  - data: opaque data sent to the transfer function 
+    //
+    function mintBatch(uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
         _mintBatch(owner(), ids, amounts, data);
     }
 
+    //
+    // getVote
+    // returns the current vote set by the administrator
+    //
+    // Returns: 
+    //  - the current voteId
+    //  - the starting time of the vote
+    //  - the ending time of the vote
+    //
     function getVote() public view returns (uint128, uint256, uint256) {
         return (_currentVoteId, _startTimestamp, _endTimestamp);
     }
 
+    //
+    // setVote
+    // sets the current vote 
+    //
+    // Parameters: 
+    //  - the current voteId
+    //  - the starting time of the vote
+    //  - the ending time of the vote
+    //
+    // The new vote must be greater than the previous ones, the starting time has to be before the end and the vote cannot happen in the past
+    //
     function setVote(uint128 voteId, uint256 start, uint256 end) public {
         require( voteId > _currentVoteId, "GovernedNFT: the new vote must have a larger id than the previous ones");
         require( end > start, "GovernedNFT: the end cannot be before the start");
@@ -89,6 +153,19 @@ contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
         _endTimestamp = end;
     }
 
+    //
+    // verify
+    // verifies a vote's signature 
+    //
+    // Parameters: 
+    //  - the ballot message
+    //  - the signature of this message
+    //
+    // Returns: true or false depending on the signature's validity
+    //
+    // The verification checks that the vote is taking place on the current vote, that the voter has not voted yet and that vote is taking place in the 
+    // defined timeframe. The function then verifies that the ballot message has been signed by the signer
+    //
     function verify(BallotMessage calldata ballot, bytes calldata signature) public view returns (bool) {
         require( ballot.voteId == _currentVoteId, "GovernedNFT: voting is not allowed on this proposal");
         require(_persons[ballot.from].lastVote < _currentVoteId, "GovernedNFT: this individual has already voted");
@@ -104,6 +181,17 @@ contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
         return signer == ballot.from;
     }
 
+    //
+    // vote
+    // vote for the current vote 
+    //
+    // Parameters: 
+    //  - the ballot message
+    //  - the signature of this message
+    //  - array of the ids on which the voter is voting
+    //
+    // Verifies the signature, stores the ballot and emits the associated event if the signature is valid
+    //
     function vote(BallotMessage calldata ballot, bytes calldata signature, uint256[] calldata ids) public {
         require(!_persons[ballot.from].blocked, "GovernedNFT: this individual is not allowed to vote");
         require(verify(ballot, signature), "GovernedNFT: invalid vote");
@@ -119,22 +207,71 @@ contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
         if (voted) _persons[ballot.from].lastVote = _currentVoteId;
     }
 
+    //
+    // hasVoted
+    // hasVoted for the current vote 
+    //
+    // Parameters: 
+    //  - address of the voter
+    //
+    // Returns the last vote to which the voter took place 
+    //
     function hasVoted(address who) public view returns (bool) {
         return _persons[who].lastVote == _currentVoteId;
     }
 
+    //
+    // isAllowed
+    // is the voter allowed to vote?
+    //
+    // Parameters: 
+    //  - address of the voter
+    //
+    // Returns true or false depending on the voter's authorization
+    //
     function isAllowed(address who) public view returns (bool) {
         return !_persons[who].blocked;
     }
 
-    function revoke(address voter) public {
+    //
+    // revoke
+    // revoke a voter 
+    //
+    // Parameters: 
+    //  - address of the voter
+    //
+    function revoke(address voter) public onlyOwner {
         _persons[voter].blocked = true;
     }
 
+    //
+    // getVotes
+    // retrieve all the ballots for a vote 
+    //
+    // Parameters: 
+    //  - id of the vote
+    //
+    // Returns:
+    //  - an array of ballots
+    //
     function getVotes(uint128 voteId) public view returns(Ballot[] memory) {
         return _ballots[voteId];
     }
 
+    //
+    // _beforeTokenTransfer
+    // internal method which is called before a transfer is taking place 
+    //
+    // Parameters: Parameters of the transfer method
+    //    address operator : who is calling for the transfer
+    //    address from : from where are the tokens transferred
+    //    address to : to where are the tokens transferred
+    //    uint256[] memory ids : array of tokens to be transferred identified by their ids
+    //    uint256[] memory amounts : array of amounts to be transferred 
+    //    bytes memory data : opaque data
+    //
+    // As the token is pausable, this function manages the pause flag for transfers
+    //
     function _beforeTokenTransfer(
         address operator,
         address from,
@@ -144,6 +281,53 @@ contract GovernedNFT is EIP712, Pausable, ERC1155URIStorage, Ownable {
         bytes memory data ) internal override {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
 
-        require(!paused(), "ERC1155Pausable: token transfer while paused");
+        require(!paused(), "GovernedNFT: token transfer while paused");
+    }
+
+    //
+    // receive
+    // method called when the smart contract receives ether 
+    //
+    // This method only signals the reception of eth. 
+    //
+    receive() external payable {
+        emit ReceivedEth(msg.sender, msg.value);
+    }
+
+    //
+    // saleRecord
+    // Records a sale on a token and emits the corresponding event
+    //
+    // Parameters: 
+    //  - tokenId: the id of token
+    //  - sale: a sales record
+    //
+    function saleRecord(uint256 tokenId_, SaleRecord calldata sale_)  public onlyOwner {
+        _sales[tokenId_] = sale_;
+        emit Sale(tokenId_, sale_.buyer, sale_.price, sale_.decimals, sale_.currency, sale_.network, sale_.status);
+    }
+
+    //
+    // saleUpdate
+    // Updates a sale on a token and emits the corresponding event
+    //
+    // Parameters: 
+    //  - tokenId: the id of token
+    //  - status: updated status 
+    //
+   function saleUpdate(uint256 tokenId_, bytes1 status_)  public onlyOwner {
+        _sales[tokenId_].status = status_;
+        emit SaleUpdate(tokenId_, _sales[tokenId_].buyer, status_);
+    }
+
+    //
+    // getSale
+    // returns a sale on a token
+    //
+    // Parameters: 
+    //  - tokenId: the id of token
+    //
+    function getSale(uint256 tokenId_) public view onlyOwner returns(SaleRecord memory) {
+        return _sales[tokenId_];
     }
 }
