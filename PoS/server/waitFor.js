742033a779c8ebf489ce61fca232bae071e1c077
GPPM03 <philippe.meyer@gazprombank.ch>
2022-09-09 12:45:01 +0200

    Preparing the merge with the server's new version

--- PoS/server/waitFor.js
+++
@@ -1,8 +0,0 @@
-export const waitFor = (conditionFunction) => new Promise((resolve) => {
-    const interval = setInterval(() => {
-        if (conditionFunction()) {
-            clearInterval(interval);
-            resolve();
-        }
-    }, 500);
-});
