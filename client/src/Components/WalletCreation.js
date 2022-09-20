aa39858426260f6a548eb56bb6256b0ed23e7736
GPPM03 <philippe.meyer@gazprombank.ch>
2022-09-20 18:15:24 +0200

    Storing the wallet in local storage

--- client/src/Components/WalletCreation.js
+++ client/src/Components/WalletCreation.js
@@ -8,24 +8,37 @@ import { Wallet } from 'ethers';
 
 export default function WalletCreation() {
     const [mnemonic, setMnemonic] = useState([]);
+    const [password, setPassword] = useState('');
 
     function getMnemonic(event) {
         setMnemonic(event.target.value.split(' '));
+    }
+    function getPassword(event) {
+        setPassword(event.target.value);
     }
 
     function validate() {
         console.log(mnemonic);
         let w = Wallet.fromMnemonic(mnemonic.join(' '));
         console.log(w.address);
+        w.encrypt(password).then((jsomWallet) => {
+            localStorage.setItem('wallet', jsomWallet);
+        })
     }
 
     return (
         <Box>
-            { (mnemonic.length == 0) ? <TextField id="mnemonic" label="Mnemonic" variant="standard" onChange={getMnemonic} /> :
-            <Grid container>
-                { mnemonic.map((word, id) => (<Grid item key={id}><TextField value={word}/></Grid> )) }
-            </Grid> }
-            { (mnemonic.length != 0) ? <Button onClick={validate}>Validate</Button> : <></>}
+            { (mnemonic.length == 0) ?
+                <Box><TextField id="mnemonic" label="Mnemonic" variant="standard" onChange={getMnemonic} /></Box>
+                :
+                <Box>
+                    <Grid container>
+                        { mnemonic.map((word, id) => (<Grid item key={id}><TextField value={word}/></Grid> )) }
+                    </Grid>
+                    <TextField id="password" label="password" variant="standard" type="password" onChange={getPassword} />
+                </Box>
+            }
+            { (mnemonic.length != 0) && (password != '')? <Button onClick={validate}>Validate</Button> : <></>}
         </Box>
     )
 }
