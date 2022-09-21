70d9cbb87788b3ec86616e12294661faa1a9aa5c
PhilMeyer <philippe@famille-meyer.net>
2022-09-21 11:19:08 +0200

    Fix on the rendering (use of useEffect)

--- client/src/Components/WalletCreation.js
+++ client/src/Components/WalletCreation.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
@@ -11,14 +11,17 @@ export default function WalletCreation({userWallet}) {
     const modeInsertMnemonic = 1;
     const modeWalletExists = 2;
     const modeCompleted = 3;
+    var walletJson;
+
 
     const [mnemonic, setMnemonic] = useState([]);
     const [password, setPassword] = useState('');
-    const [mode, setMode] = useState();
+    const [mode, setMode] = useState(-1);
 
     function getMnemonic(event) {
         setMnemonic(event.target.value.split(' '));
     }
+
     function getPassword(event) {
         setPassword(event.target.value);
     }
@@ -28,11 +31,12 @@ export default function WalletCreation({userWallet}) {
             console.log(mnemonic);
             let w = Wallet.fromMnemonic(mnemonic.join(' '));
             console.log(w.address);
-            w.encrypt(password).then((jsomWallet) => {
-                localStorage.setItem('wallet', jsomWallet);
+            w.encrypt(password).then((jsonWallet) => {
+                localStorage.setItem('wallet', jsonWallet);
+                console.log('localstorage created');
+                userWallet = w;
+                setMode(modeCompleted);
             })
-            userWallet = w;
-            setMode(modeCompleted);
         }
         else {
             userWallet = Wallet.fromEncryptedJsonSync(walletJson, password);
@@ -40,35 +44,36 @@ export default function WalletCreation({userWallet}) {
         }
     }
 
-    const walletJson = localStorage.getItem('wallet');
-    if (walletJson == null) setMode = modeInsertMnemonic;
-    else setMode(modeWalletExists);
+    useEffect(() => { 
+        walletJson = localStorage.getItem('wallet');
+        if (walletJson == null) setMode(modeInsertMnemonic);
+        else setMode(modeWalletExists);
+    }, []);
+    
+    
 
-    return (
-        { mode == modeInsertMnemonic &&
+    if(mode == modeInsertMnemonic) { return(
+        <Box>
+        { (mnemonic.length == 0) ?
+            <Box><TextField id="mnemonic" label="Mnemonic" variant="standard" onChange={getMnemonic} /></Box>
+            :
             <Box>
-            { (mnemonic.length == 0) ?
-                <Box><TextField id="mnemonic" label="Mnemonic" variant="standard" onChange={getMnemonic} /></Box>
-                :
-                <Box>
-                    <Grid container>
-                        { mnemonic.map((word, id) => (<Grid item key={id}><TextField value={word}/></Grid> )) }
-                    </Grid>
-                    <TextField id="password" label="password" variant="standard" type="password" onChange={getPassword} />
-                </Box>
-            }
-            { (mnemonic.length != 0) && (password != '')? <Button onClick={validate}>Validate</Button> : <></>}
-            </Box>
-        }
-        { mode == modeWalletExists &&
-            <Box>
+                <Grid container>
+                    { mnemonic.map((word, id) => (<Grid item key={id}><TextField value={word}/></Grid> )) }
+                </Grid>
                 <TextField id="password" label="password" variant="standard" type="password" onChange={getPassword} />
-                <Button onClick={validate}>Validate</Button>
             </Box>
         }
-        { mode == modeCompleted &&
-             <Box>
-                 <p>Wallet Loaded!</p>
-             </Box>
-        });
+        { (mnemonic.length != 0) && (password != '')? <Button onClick={validate}>Validate</Button> : <></>}
+        </Box>
+    )} else if(mode == modeWalletExists) { return (
+        <Box>
+            <TextField id="password" label="password" variant="standard" type="password" onChange={getPassword} />
+            <Button onClick={validate}>Validate</Button>
+        </Box>
+    )} else if( mode == modeCompleted) { return (
+        <Box>
+                <p>Wallet Loaded!</p>
+        </Box>
+    )}
 }
