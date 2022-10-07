461876baeebf335abe95b02fff1d59c65f682324
PhilMeyer <philippe@famille-meyer.net>
2022-10-08 00:48:48 +0200

    Fixing undefined (dies not work)

--- PoS/server/endPoints/token/collectionImage.ts
+++ PoS/server/endPoints/token/collectionImage.ts
@@ -22,12 +22,12 @@ async function collectionImage(req: Request, res: Response) {
 }
 
 async function collectionMap(req: Request, res: Response) {
-    if(req.query.id === undefined) {
+    const colId:any = req.query.id;
+    if(colId === undefined) {
         logger.info('server.collectionImage.noCollection');
         res.sendStatus(400);
         return;
     }
-    const colId:any = req.query.id;
     if (req.app.locals.collections[colId].map === undefined) {
         logger.info('server.collectionImage.undefiinedImageCollection');
         res.sendStatus(404);
