3fb589669e5f4b42cfd5282c7551f99358b193b4
PhilMeyer <philippe@famille-meyer.net>
2022-10-25 01:20:36 +0200

    initial version

---
+++ PoS/server/constants.ts
@@ -0,0 +1,15 @@
+
+export const NFT4ART_ETH_NETWORK = 1;
+export const NFT4ART_BTC_NETWORK = 2;
+export const NFT4ART_FIAT_NETWORK = 3;
+export const NFT4ART_SALE_INITIATED = 1;
+export const NFT4ART_SALE_REQUESTED_MSG = "saleRequested";
+export const NFT4ART_SALE_INITIATED_MSG = "saleInitiated";
+export const NFT4ART_SALE_ERROR_MSG = "saleError";
+export const NFT4ART_SALE_STORED_MSG = "saleStored";
+export const NFT4ART_SALE_PAID = 2;
+export const NFT4ART_SALE_PAID_MSG = "salePaid";
+export const NFT4ART_SALE_TRANSFERRED = 3;
+export const NFT4ART_SALE_TRANSFERRED_MSG = "saleTransferred";
+export const NFT4ART_ETHER = "eth";
+export const NFT4ART_BTC = "btc";
