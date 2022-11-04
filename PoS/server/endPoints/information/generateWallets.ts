a4a71f0c167cdc70fa1ba7aa3bb3c13c11f30c8d
PhilMeyer <philippe@famille-meyer.net>
2022-11-04 21:21:02 +0100

    New paper wallet

--- PoS/server/endPoints/information/generateWallets.ts
+++ PoS/server/endPoints/information/generateWallets.ts
@@ -10,7 +10,8 @@ import archiver from "archiver";
 import { config } from "../../config.js"
 
 const logo = config.galleryLogo;
-const appStoreLogo = './public/appStoreLogo.png'
+const fontBold = './public/Hershey-Noailles-Times-Simplex-Bold.ttf'
+const font = './public/Hershey-Noailles-Times-Simplex-Regular.ttf'
 
 async function generateWallets(req: Request, res: Response) {
     let nbWallets = 10;
@@ -24,7 +25,8 @@ async function generateWallets(req: Request, res: Response) {
         autoFirstPage: false,
         margins: { top: 10, bottom: 10, right: 50, left: 50 },
     });
-    var doc = new PDFDocument({ size: "A4", autoFirstPage: false });
+    var doc = new PDFDocument({ size: "A4", autoFirstPage: false, margins: { top: 72, bottom: 72, left: 72, right: 50 } });
+    const widthDoc = ((21 / 2.54) * 72) - (50 + 72)
     var tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), "nft4Art"));
     const envFile = fs.createWriteStream(path.join(tmpDir, "enveloppes.pdf"));
     const docFile = fs.createWriteStream(path.join(tmpDir, "wallets.pdf"));
@@ -39,11 +41,18 @@ async function generateWallets(req: Request, res: Response) {
     env.pipe(envFile);
     doc.pipe(docFile);
 
+    doc.registerFont('font', font);
+    doc.registerFont('fontBold', fontBold);
+    env.registerFont('font', font);
+    env.registerFont('fontBold', fontBold);
+
     for (var i = 0; i < nbWallets; i++) {
+        let x,y; 
+
         let w = Wallet.createRandom();
 
         let res = await QRCode.toFile(path.join(tmpDir, "addr" + i + ".png"), w.address);
-        env.font("Helvetica").fontSize(10);
+        env.font("font").fontSize(10);
         env.addPage();
         env.image(logo, 20, 20, { width: 120 });
         env.image(path.join(tmpDir, "addr" + i + ".png"), 400, 30, { width: 200 });
@@ -52,29 +61,43 @@ async function generateWallets(req: Request, res: Response) {
         env.moveDown(2).text((i+1) + '/' + nbWallets, { align: "right" });
 
         doc.addPage();
+        doc.font('font');
+        x = doc.x;
+        y = doc.y;
         doc.fontSize(8).text((i+1) + '/' + nbWallets, { align: "right" });
-        doc.image(logo, 20, 20, { width: 150 });
-        doc.moveDown(5);
-        doc.font("Helvetica-Bold").fontSize(20).text("Your personnal Ethereum Paper Wallet");
+        doc.fontSize(12);
+        doc.text('The Lighthouse', x, y).moveDown(0.3);
+        doc.text('Raemistrasse 5').moveDown(0.3);
+        doc.text('8001 Zürich').moveDown(0.3);
+        doc.text('mail@lighthouse.com').moveDown(0.3);
+        doc.moveDown(4);
+        doc.font('fontBold').fontSize(30).text('Puzzle of a Maze', {align: 'center'});
+        doc.fontSize(12).text('by Thomas Julier', {align: 'center'});
+        doc.moveDown(4);
+        doc.text("Your personnal Ethereum Paper Wallet");
         doc.moveDown(1);
-        doc.font("Times-Roman").fontSize(12);
+        doc.font('font');
         doc.text(
-            "Insert the mnemonic words below in any Ethereum compatible wallet to access your digital artwork by Thomas Julier. Treat this as a password and do not share it with anyone."
+            "Insert the mnemonic words below in any Ethereum compatible wallet to access your digital artwork. Treat this as a password and do not share it with anyone."
         );
         doc.moveDown(2);
-        doc.font("Times-Bold").fontSize(12).text(w.mnemonic.phrase);
+        doc.font("fontBold");
+        let words = w.mnemonic.phrase.split(' ');
+        doc.text(words[0] + ' ' + words[1] + ' ' + words[2] + ' ' + words[3], { align: 'center'});
+        doc.text(words[4] + ' ' + words[5] + ' ' + words[6] + ' ' + words[7], { align: 'center'});
+        doc.text(words[8] + ' ' + words[9] + ' ' + words[10] + ' ' + words[11], { align: 'center'});
         doc.moveDown(2);
-        doc.fontSize(12).text("The Ethereum address of this wallet is: ", { continued: true });
-        doc.font("Courier").fontSize(12).text(w.address);
+        doc.font('font').text("The Ethereum address of this wallet is:");
+        doc.moveDown(1)
+        doc.font("Courier").fontSize(12).text(w.address, {align: 'center'});
         let qr = await QRCode.toFile(path.join(tmpDir, "phrase" + i + ".png"), w.mnemonic.phrase);
         doc.moveDown(2);
-        doc.font("Times-Roman").fontSize(12).text("For an overview and co-governance of the artwork, install the companion App scanning the QR code below:");
-        let tempX = doc.x;
-        let tempY = doc.y;
-        doc.image(path.join(tmpDir, 'installationQRcode.png'), doc.x, doc.y, {width: 150});
-        doc.image(appStoreLogo, tempX + 150, tempY + 50, {width: 100});
-        doc.moveDown(2).text("And then, we running the companion app, scan the QR code below to insert your wallet");
-        doc.image(path.join(tmpDir, "phrase" + i + ".png"), doc.x, doc.y, {width: 150});
+        doc.font("font").text('Explore the artwork through the companion webApp developed by Philippe and Laurent Meyer. To access the webApp, scan the following QR-code:');
+        doc.moveDown(0.5);
+        doc.image(path.join(tmpDir, 'installationQRcode.png'), doc.x + (widthDoc -100) / 2, doc.y, {width: 100});
+        doc.moveDown(2).text('To access your digital artwork and unlock its co-governance features, scan the following QR-code within the companion webApp:');
+        doc.moveDown(0.5);
+        doc.image(path.join(tmpDir, "phrase" + i + ".png"), doc.x + (widthDoc -100) / 2, doc.y, {width: 100});
     }
     
     doc.end();
